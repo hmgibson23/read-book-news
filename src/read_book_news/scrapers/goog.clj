32baa5371e78a8c27606e72a7fd7b75a62d2b7c0
Hugo Gibson <gibsonhugo@gmail.com>
2014-04-23 12:25:56 -0500

    Create nice map of passed symbol from scraped Google data

--- src/read_book_news/scrapers/goog.clj
+++ src/read_book_news/scrapers/goog.clj
@@ -53,5 +53,12 @@
 
 ; create a map, containing all the relevant information
 ; about the passed symbol
-(defn package-quote [symbol])
+(defn package-quote [symbol]
+  {:symbol 'symbol
+   :name (get-name symbol)
+   :price (get-price symbol)
+   :eps (get-eps symbol)
+   :pe (get-pe symbol)
+   :quote-time (get-quotetime symbol)})
+
 
