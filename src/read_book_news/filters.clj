428eb4f59f982b2be9654a4248fca1a97d3d418f
Hugo Gibson <gibsonhugo@gmail.com>
2014-04-24 16:22:03 -0500

    No more

--- src/read_book_news/filters.clj
+++ src/read_book_news/filters.clj
@@ -1,5 +1,15 @@
 (ns read-book-news.filters)
 
 ; Some filters for parsing our stocks
+
+
+(defn coll-filter [key upper lower coll]
+  (let [k (keyword key)]
+    (filter (fn [x] (and (< (k x) upper) (> (k x) lower))) coll)))
+
 (defn p-e [upper lower coll]
-  (filter (fn [x] (and (< (:pe x) upper) (> (:pe x) lower))) coll))
+  (coll-filter "pe" upper lower coll))
+
+(defn peg [upper lower coll]
+  (coll-filter "peg" upper lower coll))
+
