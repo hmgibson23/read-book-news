1a976c1d86a34ab8e270eda372d310dc6463acda
Hugo Gibson <gibsonhugo@gmail.com>
2014-04-25 16:23:46 -0500

    Added reutters URL

--- src/read_book_news/scrapers/reutters.clj
+++ src/read_book_news/scrapers/reutters.clj
@@ -2,3 +2,6 @@
 
 ; Get information from Reutters
 ; only grab key financials from them
+
+(def financial-highlights "http://www.reuters.com/finance/stocks/financialHighlights?symbol=")
+(def financial-statement "http://www.reuters.com/finance/stocks/incomeStatement?symbol=")
